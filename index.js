059f08a9e4ce367f8d828a2ef720de767c76910c
brunnacroches <brunnacroches@gmail.com>
2021-12-14 18:50:22 -0300

    correções

--- index.js
+++ index.js
@@ -3,180 +3,181 @@ import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from './three.js-master/examples/jsm/controls/OrbitControls.js'
 import Stats from './three.js-master/examples/jsm/libs/stats.module.js';
 
-	const statsEnabled = true;
+const statsEnabled = true;
 
-	let container, stats, loader, clock;
+		let container, stats, loader, clock;
 
-	let camera, scene, renderer, mousemove, autoRotate;
+		let camera, scene, renderer, elf, mesh;
 
-	let spotLight;
+		let spotLight;
 
-	let mouseX = 0;
-	let mouseY = 0;
+		let mouseX = 0;
+		let mouseY = 0;
 
-	let targetX = 0;
-	let targetY = 0;
+		let targetX = 0;
+		let targetY = 0;
 
-	// let autoRotate = null;
-	// let mousemove = null;
+		const windowHalfX = window.innerWidth / 2;
+		const windowHalfY = window.innerHeight / 2;
 
-	const windowHalfX = window.innerWidth / 2;
-	const windowHalfY = window.innerHeight / 2;
+			init();
+			animate();
 
-	init();
-	animate();
+			function init() {
 
-	function init() {
+				container = document.getElementById('container');
+				// document.body.appendChild(container);
 
-		container = document.getElementById('container');
-		// document.body.appendChild(container);
-
-		camera = new THREE.PerspectiveCamera( 75,  window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.set(1, 1, 4);
-		camera.lookAt( 0, 3, 0 );
+			  camera = new THREE.PerspectiveCamera( 75,  window.innerWidth / window.innerHeight, 1, 100)
+        camera.position.set(1, 1, 4)
+				camera.lookAt( 0, 3, 0 );
         
-    // clock = new THREE.Clock();
-		scene = new THREE.Scene();
-		scene.background = new THREE.Color( 0x060708 );
-		clock = new THREE.Clock();
+        clock = new THREE.Clock();
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0x060708 );
 
-				                  // ADD O ARQUIVO GLB 
-    loader = new GLTFLoader()
-    loader.load('assets/my--avatar.glb', function(glb) {
-				mousemove = glb.scene;
-				autoRotate = glb.scene;
-        mousemove.scale.set(0.2, 0.2, 0.2)
-				autoRotate.scale.set(0.2, 0.2, 0.2)
-        scene.add(mousemove);
-				scene.add(autoRotate);
-      
-			});
+
+				// loading manager
+				const loadingManager = new THREE.LoadingManager( function () {
+
+					scene.add(elf);
+
+				} );
+
 				// LIGHTS
 
-		scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
+				scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
 
-		spotLight = new THREE.SpotLight( 0xffffbb, 2 );
-		spotLight.position.set( 5,5,5);
-		spotLight.position.multiplyScalar( 700 );
-		scene.add( spotLight );
+				spotLight = new THREE.SpotLight( 0xffffbb );
+				spotLight.position.set(1,1,1);
+				spotLight.position.multiplyScalar( 100 );
+				scene.add( spotLight );
 				
 
-		spotLight.castShadow = true;
+				spotLight.castShadow = true;
 
-		spotLight.shadow.mapSize.width = 2048;
-		spotLight.shadow.mapSize.height = 2048;
+				spotLight.shadow.mapSize.width = 1000;
+				spotLight.shadow.mapSize.height = 1000;
 
-		spotLight.shadow.camera.near = 200;
-		spotLight.shadow.camera.far = 1500;
+				spotLight.shadow.camera.near = 1000;
+				spotLight.shadow.camera.far = 1000;
 
-		spotLight.shadow.camera.fov = 40;
+				spotLight.shadow.camera.fov = 40;
 
-		spotLight.shadow.bias = - 0.005;
-		
-		//
+				spotLight.shadow.bias = - 0.005;
 
-		renderer = new THREE.WebGLRenderer();
-		renderer.setPixelRatio( window.devicePixelRatio );
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		container.appendChild( renderer.domElement );
+                  // ADD O ARQUIVO GLB 
+          loader = new GLTFLoader(loadingManager)
+          loader.load('assets/my--avatar.glb', function(glb){
+              createScene(glb.scene.mesh, 100 );
+              createScene(glb.scene.elf, 100 );
+              mesh = glb.scene;
+              elf = glb.scene;
+              mesh.scale.set(0.2, 0.2, 0.2)
+              scene.add(mesh);
+							scene.add(elf);
+          })
 
-		renderer.shadowMap.enabled = true;
-		renderer.outputEncoding = THREE.sRGBEncoding;
 
-		//
-					
-		stats = new Stats();
-		container.appendChild( stats.dom );
-				
-		//
+				renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
 
-		const controls = new OrbitControls(camera, renderer.domElement)
-		controls.enableDamping = false
-		controls.enableZoom= true
-		controls.enablePan= true
-		controls.dampingFactor= false
-		controls.minDistance= 4
-		controls.maxDistance= 5
-		// controls.autoRotate
-		// 		controls.zoomSpeed= 10
-		// 		controls.autoRotateSpeed= 0.5
-		// 		controls.rotateSpeed= -1.4
+				renderer.shadowMap.enabled = true;
+				renderer.outputEncoding = THREE.sRGBEncoding;
+
+				//
 
 				// if ( statsEnabled ) {
 
-						
-}
+					stats = new Stats();
+					container.appendChild( stats.dom );
 
-							// EVENTS
-				// resize = redimensionar  
-	function onWindowResize() {
+			// }
 
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-				
-		renderer.setSize( window.innerWidth, window.innerHeight );
-}
+				// EVENTS
 
-		autoRotate = true;
-		mousemove = true;
+				window.addEventListener( 'mousemove', onWindowMouseMove );
+				window.addEventListener( 'resize', onWindowResize );
 
-		// const autoRotate = autoRotate;
-		// const mousemove = mousemove;
+			}
 
-		window.addEventListener( 'resize', onWindowResize );
-		window.addEventListener( 'mousemove', onWindowMouseMove);
-		window.addEventListener( 'autoRotate', onWindowAutoRotate);
+      const controls = new OrbitControls(camera, renderer.domElement)
+			controls.enableDamping = false
+			controls.enableZoom= true
+			controls.enablePan= true
+			controls.dampingFactor= false
+			controls.minDistance= 4
+			controls.maxDistance= 5
+			// controls.autoRotate
+			// 		controls.zoomSpeed= 10
+			// 		controls.autoRotateSpeed= 0.5
+			// 		controls.rotateSpeed= -1.4
 
+			function createScene( geometry, scale, material) {
 
-	function onWindowAutoRotate (event) {
+				mesh = new THREE.Mesh( geometry, material );
 
-		autoRotate = true;
-		mousemove = false;
-		console.log("Auto Rotate");
+				mesh.position.y = -50;
+				mesh.scale.set( scale, scale, scale );
 
-		mouseX = ( event.clientX - windowHalfX );
-		mouseY = ( event.clientY - windowHalfY );
+				mesh.castShadow = true;
+				mesh.receiveShadow = true;
 
-	}
+				scene.add( mesh );
 
-	function onWindowMouseMove(event) {
-		autoRotate = false;
-		mousemove = true;
-		console.log("Mouse Move");
+			}
 
-		mouseX = ( event.clientX - windowHalfX );
-		mouseY = ( event.clientY - windowHalfY );
-		
-	}
 			//
-	function animate() {
-		
-		requestAnimationFrame( animate );
-		render();
-		// stats.update();
-		
-	}
 
-	function render() {
+			function onWindowResize() {
 
-		const delta = clock.getDelta();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
 
-		targetX = mouseX * .001;
-		targetY = mouseY * .001;
+			}
+ 
+			function onWindowMouseMove( event ) {
 
-		if ( autoRotate ) {	
-				autoRotate.rotation.y += delta * 0.5;
-}
-		if (mousemove ) {
-				mousemove.rotation.y += delta * 0.0;
-}
-//
-		if ( mousemove ) {
-				mousemove.rotation.y += 0.25 *  ( targetX - mousemove.rotation.y );
-				mousemove.rotation.x += 0.25 *  ( targetY - mousemove.rotation.x );
-		} else {
-				mousemove.rotation.y += 0.0;
-}
-		//
-		renderer.render( scene, camera );
-};
+				mouseX = ( event.clientX - windowHalfX );
+				mouseY = ( event.clientY - windowHalfY );
+
+			}
+
+			//
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				render();
+				if ( statsEnabled ) 
+        stats.update();
+
+			}
+
+			function render() {
+
+				targetX = mouseX * .001;
+				targetY = mouseY * .001;
+
+				if ( mesh ) {
+
+					mesh.rotation.y += 0.05 * ( targetX - mesh.rotation.y );
+					mesh.rotation.x += 0.05 * ( targetY - mesh.rotation.x );
+
+				}
+
+        const delta = clock.getDelta();
+
+				if ( elf !== undefined ) {
+
+					elf.rotation.y += delta * 0.2;
+
+				}
+
+				renderer.render( scene, camera );
+
+			}
