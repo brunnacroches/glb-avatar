bcac3a40dd5dba7bc32b8871d2b006390d855ad4
brunnacroches <brunnacroches@gmail.com>
2021-12-14 16:36:58 -0300

    organize

--- index.js
+++ index.js
@@ -3,186 +3,152 @@ import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from './three.js-master/examples/jsm/controls/OrbitControls.js'
 import Stats from './three.js-master/examples/jsm/libs/stats.module.js';
 
-const statsEnabled = true;
+	const statsEnabled = true;
 
-			let container, stats, loader;
+	let container, stats, loader;
 
-			let camera, scene, renderer, elf, mesh;
+	let camera, scene, renderer, root;
 
-			let spotLight;
+	let spotLight;
 
-			let mouseX = 0;
-			let mouseY = 0;
+	let mouseX = 0;
+	let mouseY = 0;
 
-			let targetX = 0;
-			let targetY = 0;
+	let targetX = 0;
+	let targetY = 0;
 
-			const windowHalfX = window.innerWidth / 2;
-			const windowHalfY = window.innerHeight / 2;
+	const windowHalfX = window.innerWidth / 2;
+	const windowHalfY = window.innerHeight / 2;
 
-			init();
-			animate();
+	init();
+	animate();
 
-			function init() {
+	function init() {
 
-				container = document.getElementById('container');
-				// document.body.appendChild(container);
+		container = document.getElementById('container');
+		// document.body.appendChild(container);
 
-			  camera = new THREE.PerspectiveCamera( 75,  window.innerWidth / window.innerHeight, 1, 10000)
-        camera.position.set(1, 1, 4)
-				camera.lookAt( 0, 3, 0 );
+		camera = new THREE.PerspectiveCamera( 75,  window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.set(1, 1, 4);
+		camera.lookAt( 0, 3, 0 );
         
-        // clock = new THREE.Clock();
-				scene = new THREE.Scene();
-				scene.background = new THREE.Color( 0x060708 );
+    // clock = new THREE.Clock();
+		scene = new THREE.Scene();
+		scene.background = new THREE.Color( 0x060708 );
 
-
-				// loading manager
-				const loadingManager = new THREE.LoadingManager( function () {
-
-					scene.add(elf);
-
-				} );
-
+				                  // ADD O ARQUIVO GLB 
+    loader = new GLTFLoader()
+    loader.load('assets/my--avatar.glb', function(glb) {
+        root = glb.scene;
+        root.scale.set(0.2, 0.2, 0.2)
+        scene.add(root);
+      
+			});
 				// LIGHTS
 
-				scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
+		scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
 
-				spotLight = new THREE.SpotLight( 0xffffbb, 2 );
-				spotLight.position.set( 5,5,5);
-				spotLight.position.multiplyScalar( 700 );
-				scene.add( spotLight );
+		spotLight = new THREE.SpotLight( 0xffffbb, 2 );
+		spotLight.position.set( 5,5,5);
+		spotLight.position.multiplyScalar( 700 );
+		scene.add( spotLight );
 				
 
-				spotLight.castShadow = true;
+		spotLight.castShadow = true;
 
-				spotLight.shadow.mapSize.width = 2048;
-				spotLight.shadow.mapSize.height = 2048;
+		spotLight.shadow.mapSize.width = 2048;
+		spotLight.shadow.mapSize.height = 2048;
 
-				spotLight.shadow.camera.near = 200;
-				spotLight.shadow.camera.far = 1500;
+		spotLight.shadow.camera.near = 200;
+		spotLight.shadow.camera.far = 1500;
 
-				spotLight.shadow.camera.fov = 40;
+		spotLight.shadow.camera.fov = 40;
 
-				spotLight.shadow.bias = - 0.005;
+		spotLight.shadow.bias = - 0.005;
+		
+		//
 
+		renderer = new THREE.WebGLRenderer();
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		container.appendChild( renderer.domElement );
 
-                  // ADD O ARQUIVO GLB 
-          loader = new GLTFLoader(loadingManager)
-          loader.load('assets/my--avatar.glb', function(glb){
-              // createScene(glb.scene.mesh, 100 );
-              // createScene(glb.scene.elf, 100 );
+		renderer.shadowMap.enabled = true;
+		renderer.outputEncoding = THREE.sRGBEncoding;
 
-              mesh = glb.scene;
-              elf = glb.scene;
-              mesh.scale.set(0.2, 0.2, 0.2)
-              scene.add(mesh);
-							scene.add(elf);
-          })
+		//
+					
+		stats = new Stats();
+		container.appendChild( stats.dom );
+				
+		//
 
-
-				renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				container.appendChild( renderer.domElement );
-
-				renderer.shadowMap.enabled = true;
-				renderer.outputEncoding = THREE.sRGBEncoding;
-
-				//
+		const controls = new OrbitControls(camera, renderer.domElement)
+		controls.enableDamping = false
+		controls.enableZoom= true
+		controls.enablePan= true
+		controls.dampingFactor= false
+		controls.minDistance= 4
+		controls.maxDistance= 5
+		controls.autoRotate
+				// controls.zoomSpeed= 10
+				// controls.autoRotateSpeed= 0.5
+				// controls.rotateSpeed= -1.4
 
 				// if ( statsEnabled ) {
 
-					stats = new Stats();
-					container.appendChild( stats.dom );
+								// EVENTS
+				// resize = redimensionar  
 
-			// }
+		window.addEventListener( 'mousemove', onWindowMouseMove );
+		window.addEventListener( 'resize', onWindowResize );
+}
+				//
+			
+	function onWindowResize() {
 
-				// EVENTS
-
-				window.addEventListener( 'mousemove', onWindowMouseMove );
-				window.addEventListener( 'resize', onWindowResize );
-
-			}
-
-
-      const controls = new OrbitControls(camera, renderer.domElement)
-      controls.enableDamping = false
-			controls.enableZoom= true
-			controls.enablePan= true
-			controls.dampingFactor= false
-			controls.minDistance= 4
-			controls.maxDistance= 5
-			controls.autoRotate
-			// controls.zoomSpeed= 10
-			// controls.autoRotateSpeed= 0.5
-			// controls.rotateSpeed= -1.4
-
-			// function createScene( geometry, scale) {
-
-			// 	mesh = new THREE.Mesh( geometry );
-
-			// 	mesh.position.y = - 50;
-			// 	mesh.scale.set( scale, scale, scale );
-
-			// 	mesh.castShadow = true;
-			// 	mesh.receiveShadow = true;
-
-			// 	scene.add( mesh );
-
-			// }
-
-			//
-
-			function onWindowResize() {
-
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-
-			}
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+				
+		renderer.setSize( window.innerWidth, window.innerHeight );
+}
  
-			function onWindowMouseMove( event ) {
+	function onWindowMouseMove( event ) {
 
-				event.preventDefault();
-				mouseX = ( event.clientX - windowHalfX );
-				mouseY = ( event.clientY - windowHalfY );
+		event.preventDefault();
+		mouseX = ( event.clientX - windowHalfX );
+		mouseY = ( event.clientY - windowHalfY );
 
-			}
-
+	}
 			//
 
-			function animate() {
+	function animate() {
 
-				requestAnimationFrame( animate );
+		requestAnimationFrame( animate );
 
-				render();
-				if ( statsEnabled ) 
-        stats.update();
+		render();
+		if ( statsEnabled );
+    stats.update();
 
-			}
+	}
 
-			function render() {
-				// var delta = clock.getDelta();
+	function render() {
 
-				targetX = mouseX * .001;
-				targetY = mouseY * .001;
+		targetX = mouseX * .001;
+		targetY = mouseY * .001;
 
-				if ( mesh ) {
+		if ( root ) {
 
-					mesh.rotation.y += 0.25 * ( targetX - mesh.rotation.y );
-					mesh.rotation.x += 0.25 * ( targetY - mesh.rotation.x );
+			root.rotation.y += 0.25 * ( targetX - root.rotation.y );
+			root.rotation.x += 0.25 * ( targetY - root.rotation.x );
 
-				if ( elf ) {
-						// elf.rotation.y += delta * 0.2;
+		if ( root ) {
+						// root.rotation.y += delta * 0.2;
 	
-					elf.rotation.y += 0.2 * (targetX - elf.rotation.y);
-					} else {
-						elf.rotation.y = time 
-					}
-				}
-
-
-				renderer.render( scene, camera );
-
-			}
+			root.rotation.y += 0.2 * (targetX - root.rotation.y);
+			} else 
+				root.rotation.y = time 
+					
+		renderer.render( scene, camera );
+		}
+}
